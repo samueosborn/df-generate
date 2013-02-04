b15527b87c23dd89f4bdabd00245a3f6a60b7b4f
Daniel Devine <devine@ddevnet.net>
2013-02-04 15:19:34 +1000

    Progress. Also, its not really a "For" but rather a "Thanks to".

--- jquery.dform-generate.js
+++ jquery.dform-generate.js
@@ -3,15 +3,43 @@
         // This function takes a HTML form and generates JSON compatible with jQuery.dForm.
         // This is useful if you pair it with an automatic form builder tool - or if your designer 
         // crafts forms in some IDE (terrible!) which you have to translate.
-        var form = this;
+        var form = this; // As documentation says, no need to wrap: $(this).
         var outobj = {}; // An object to store the data we extract. JSON stringify later.
+
+        // We should not proceed if we're not handed a Form object.
+        if(form.prop("tagName").toLowerCase() != "form"){ // I screwed around with .attr() for ages.
+            console.log("Not a form!");
+            return null; // Does returning null make sense? I'll find out.
+        }
 
         // For each child push piece of JSON into the output.
         form.children().each(function() {
             console.log(this);
-        });
-    };    
+
+            var type = this.tagName.toLowerCase();
+            switch(type){
+                case "text":
+                    console.log("type: text");
+                    break;
+
+                case "input":
+                    console.log("type: text");
+                    break;
+
+                case "br":
+                    console.log("type: br");
+                    break;
+
+                case "div":
+                    console.log("type: div");
+                    break;
+                default:
+                    console.log("Unknown element type: " + type);
+            }
+
+        }); // End foreach
+    }; // End function   
 })( jQuery );
 
-// For Taryn, who set me on my feet, gave me my motivation back and who still leaves me in awe every
+// This is thanks to Taryn, who set me on my feet, gave me my motivation back and who still leaves me in awe every
 // day. -- Daniel Devine
