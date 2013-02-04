5e312066a95a1f76f7616f0152f5dc803fabf895
Daniel Devine <devine@ddevnet.net>
2013-02-04 13:30:07 +1000

    Initial commit.

---
+++ jquery.dform-generate.js
@@ -0,0 +1,12 @@
+(function ( $ ){
+    $.fn.dform-generate = function(){
+        // This function takes a HTML form and generates JSON compatible with jQuery.dForm.
+        // This is useful if you pair it with an automatic form builder tool - or if your designer 
+        // crafts forms in some IDE (terrible!) which you have to translate.
+
+
+    }
+})( jQuery );
+
+// For Taryn, who set me on my feet, gave me my motivation back and who still leaves me in awe every
+// day. -- Daniel Devine
