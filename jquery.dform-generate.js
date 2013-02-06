c0ea5e3cc4da565ce98d12f4f760f15edda887d3
Daniel Devine <devine@ddevnet.net>
2013-02-06 15:40:51 +1000

    It's alive! Just needs fleshing out now.

--- jquery.dform-generate.js
+++ jquery.dform-generate.js
@@ -9,7 +9,7 @@
 
         // We should not proceed if we're not handed a Form object.
         if (form.prop("tagName").toLowerCase() != "form"){
-            console.log("Not a form!");
+            //console.log("Not a form!");
             return null; // Does returning null make sense? I'll find out.
         }
 
@@ -17,51 +17,52 @@
         storeAttributes(form[0], formobj);
         formobj.html = getContent(form[0]);
 
-        console.log(JSON.stringify(formobj));
         return JSON.stringify(formobj); //Just use JSON.stringify until things are working well. 
         // Crockford json2.js prototypes over JSON.stringify!
 
         function getChildNodes(node){
             // If there are multiple children, an array of objects will be returned, else just 1.
-            console.log("getChildNodes: " + node.id);
+            //console.log("getChildNodes: " + node.id);
             
-            children = $([]);
+            var children = $([]);
             $(node).children().each(function(){
                 if (nodeseen.indexOf(this) == -1){
                     children.push(this);
                 }
             });
 
-            if  (children.length > 1){ // Was 0, if behavior strange look at this.
+            if  (children.length > 1 ){
                 // Return an array of children.
-                result = [];
+                var result = [];
                 children.each(function(){
-                    child = {};
+                    var child = {};
                     // Get attributes for section. These are k:v pair attributes.
                     storeAttributes(this, child);
                     // Get content (which may contain child nodes).
-                    child.html = getContent(this); // JSON.stringify should ignore undefined values.
+                    child.html = getContent(this);
                     result.push(child);
                 });
                 return result;
-            } else {
+            } else if (children.length == 1){
                 // Return a single node, which may have a single child under the html attribute.
-                nodeobj = {};
+                var nodeobj = {};
                 storeAttributes(node, nodeobj);
                 nodeobj.html = getContent(node);
                 return nodeobj;
+            } else {
+                return null;
             }
         }
 
         function getContent(node){
             // Returns the contents for a node... This may be a string or child(ren).
-            children = $(node).children();
-            console.log("getContent children: [" + node.id + "] " + children.length);
+            var children = $(node).children();
+            //console.log("getContent children: [" + node.id + "] " + children.length);
             if (children.length == 0){
-                return node.innerHTML;
+                return (node.innerHTML != "") ? node.innerHTML : undefined; // JSON.stringify should ignore undefined values.
             } else {
                 var res = getChildNodes(node);
-                console.log("getContent recursed into: [" + node.id + "] " + JSON.stringify(res));
+                //console.log("getContent recursed into: [" + node.id + "] yielding " + JSON.stringify(res));
                 return res;
             }
         }
@@ -70,7 +71,7 @@
             // Store attributes of a node in the storage object.
 
             nodeseen.push(node);
-            console.log("Seen: [" + nodeseen.length + "] " +node);
+            //console.log("Seen: [" + nodeseen.length + "] " + node + " id: " + node.id);
 
             var tag = $(node).prop("tagName").toLowerCase();
             var type = node.type;
@@ -78,14 +79,14 @@
             // All elements have a tagName, but not all have a type.
             switch(tag){
                 case "label":
-                    console.log("tag: label");
+                    //console.log("tag: label");
                     storage.type = type;
                     storage.id = node.id;
                     storage.name = $(node).attr("for");
                     break;
 
                 case "p":
-                    console.log("tag: p");
+                    //console.log("tag: p");
                     storage.type = "p";
                     storage.id = node.id;
                     break;
@@ -93,7 +94,7 @@
                 case "input": 
                     // Note: we output "text" (a type attribute of HTMLInputElement), though "input" 
                     // works and is equally valid?
-                    console.log("tag: input");
+                    //console.log("tag: input");
                     storage.type = type;
                     storage.id = node.id;
                     storage.name = node.name;
@@ -101,19 +102,19 @@
                     break;
 
                 case "br":
-                    console.log("tag: br");
+                    //console.log("tag: br");
                     storage.type = "br";
                     break;
 
                 case "div":
-                    console.log("tag: div");
+                    //console.log("tag: div");
                     storage.type = "div";
                     storage.id = node.id;
                     storage.name = node.name;
                     break;
 
                 case "form":
-                    console.log("tag: form");
+                    //console.log("tag: form");
                     storage.action = node.action;
                     storage.method = node.method;
                     storage.id = node.id;
@@ -121,11 +122,11 @@
                     break;
 
                 case "a":
-                    console.log("tag: a");
+                    //console.log("tag: a");
                     storage.href = node.href;
                     storage.id = node.id;
                 default:
-                    console.log("Unknown element tag: " + tag + " with type: "  + type);
+                    //console.log("Unknown element tag: " + tag + " with type: "  + type);
             }
             return;
         } // End storeAttributes
